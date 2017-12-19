2ab76304fc688d053d242619c9d8d58d7de246a4
middtown <aarontm9@gmail.com>
2017-12-19 07:56:09 -0700

    form works with update to page

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -1,6 +1,7 @@
 //pull from api and load page with info
 $.get("https://den-super-crud.herokuapp.com/books").done(function(data) {
     console.log(data);
+    	//loop through and get infe through index and val.somgthing
     	$.each(data.books, function( i, val) {
 			$("#books").append("<li>" + val.title + "<br>" + val.author + "<br>" +
 		 	val.releaseDate + "<br>" + "<img src= '" + val.image + "' >" + "</li>");		
@@ -17,7 +18,7 @@ $.get("https://den-super-crud.herokuapp.com/books").done(function(data) {
 		var formAuthor =  $("#book-author").val();
 		var formReleaseDate =  $("#book-release-date").val();
 		var formImage =  $("#book-image").val();
-
+		//new book object.
 		var newBook = {
 			// title here
 			title : formTitle,
@@ -33,7 +34,17 @@ $.get("https://den-super-crud.herokuapp.com/books").done(function(data) {
 		console.log(formReleaseDate);	
 		console.log(formImage);	
 						
-	$.post("https://den-super-crud.herokuapp.com/books", newBook);
-					     
+	// $.post("https://den-super-crud.herokuapp.com/books", newBook);
+
+		//post to screen using ajax an d success function to post to page. 
+		$.ajax({
+  			type: "POST",
+ 			url: "https://den-super-crud.herokuapp.com/books",
+ 			data: newBook,
+ 			success: function() {
+      			$("#books").append("<li>" + formTitle + "<br>" + formAuthor + "<br>" +
+		 	formReleaseDate + "<br>" + "<img src= '" + formImage + "' >" + "</li>");		
+			}
+		});			     
     });
 
