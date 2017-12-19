c75c2dc3bbc4eca22536e9e259a1f0f9d489850d
middtown <aarontm9@gmail.com>
2017-12-18 20:39:19 -0700

    everything works except for not refreshing... need ajax code for that

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -3,14 +3,37 @@ $.get("https://den-super-crud.herokuapp.com/books").done(function(data) {
     console.log(data);
     	$.each(data.books, function( i, val) {
 			$("#books").append("<li>" + val.title + "<br>" + val.author + "<br>" +
-		 	val.releaseDate + "<br>" + "<img src=" + val.image + ">" + "</image> </li> <br>");		
+		 	val.releaseDate + "<br>" + "<img src= '" + val.image + "' >" + "</li>");		
 		});
 });
 
-//form submit section 
-$("form").on("submit", function(event){
-		    // Stop the form from submitting
-			event.preventDefault();
-	console.log("I've been clicked!");
-  	alert( "Handler for .click() called." );
-});
+	//form submit section 
+	$("#newBook").on("submit", function(event){
+		// Stop the form from submitting
+		event.preventDefault();
+		console.log("I've been clicked!");
+
+		var formTitle = $("#book-title").val();		
+		var formAuthor =  $("#book-author").val();
+		var formReleaseDate =  $("#book-release-date").val();
+		var formImage =  $("#book-image").val();
+
+		var newBook = {
+			// title here
+			title : formTitle,
+			// author here
+			author: formAuthor,
+			// image URL here
+			image: formImage,
+			// release date here
+			releaseDate: formReleaseDate
+		};
+		console.log(formTitle);	
+		console.log(formAuthor);	
+		console.log(formReleaseDate);	
+		console.log(formImage);	
+						
+	$.post("https://den-super-crud.herokuapp.com/books", newBook);
+					     
+    });
+
